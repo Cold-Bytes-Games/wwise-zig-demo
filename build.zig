60eac3eafb79f57cb5066d485a179375c1974f4b
mlarouche <michael.larouche@gmail.com>
2023-05-26 15:36:35 -0400

    Using zig-gamedev to create the UI

--- build.zig
+++ build.zig
@@ -1,5 +1,10 @@
 const std = @import("std");
 
+const zglfw = @import("vendor/zig-gamedev/libs/zglfw/build.zig");
+const zgpu = @import("vendor/zig-gamedev/libs/zgpu/build.zig");
+const zgui = @import("vendor/zig-gamedev/libs/zgui/build.zig");
+const zpool = @import("vendor/zig-gamedev/libs/zpool/build.zig");
+
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
@@ -11,6 +16,27 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
     b.installArtifact(exe);
+
+    const zglfw_pkg = zglfw.package(b, target, optimize, .{});
+    const zgui_pkg = zgui.package(b, target, optimize, .{
+        .options = .{
+            .backend = .glfw_wgpu,
+        },
+    });
+    const zpool_pkg = zpool.package(b, target, optimize, .{});
+    const zgpu_pkg = zgpu.package(b, target, optimize, .{
+        .options = .{ .uniforms_buffer_size = 4 * 1024 * 1024 },
+        .deps = .{ .zpool = zpool_pkg.zpool, .zglfw = zglfw_pkg.zglfw },
+    });
+
+    exe.addModule("zglfw", zglfw_pkg.zglfw);
+    exe.addModule("zgpu", zgpu_pkg.zgpu);
+    exe.addModule("zgui", zgui_pkg.zgui);
+    exe.addModule("zpool", zpool_pkg.zpool);
+
+    zglfw_pkg.link(exe);
+    zgpu_pkg.link(exe);
+    zgui_pkg.link(exe);
 
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
--- src/main.zig
+++ src/main.zig
@@ -1,17 +1,133 @@
 const std = @import("std");
+const zglfw = @import("zglfw");
+const zgui = @import("zgui");
+const zgpu = @import("zgpu");
+
+const DemoState = struct {
+    graphics_context: *zgpu.GraphicsContext,
+    is_selected: bool = false,
+};
+
+fn setup(allocator: std.mem.Allocator, window: *zglfw.Window) !*DemoState {
+    const graphics_context = try zgpu.GraphicsContext.create(allocator, window);
+
+    zgui.init(allocator);
+
+    const scale_factor = scale_factor: {
+        const scale = window.getContentScale();
+        break :scale_factor std.math.max(scale[0], scale[1]);
+    };
+
+    zgui.backend.initWithConfig(
+        window,
+        graphics_context.device,
+        @enumToInt(zgpu.GraphicsContext.swapchain_format),
+        .{
+            .texture_filter_mode = .linear,
+            .pipeline_multisample_count = 1,
+        },
+    );
+
+    const style = zgui.getStyle();
+    style.scaleAllSizes(scale_factor);
+
+    const demo = try allocator.create(DemoState);
+    demo.* = .{
+        .graphics_context = graphics_context,
+    };
+    return demo;
+}
+
+fn destroy(allocator: std.mem.Allocator, demo: *DemoState) void {
+    zgui.backend.deinit();
+    zgui.deinit();
+    demo.graphics_context.destroy(allocator);
+    allocator.destroy(demo);
+}
+
+const Languages = &[_][:0]const u8{ "English(US)", "French(Canada)" };
+
+fn update(demo: *DemoState) void {
+    zgui.backend.newFrame(
+        demo.graphics_context.swapchain_descriptor.width,
+        demo.graphics_context.swapchain_descriptor.height,
+    );
+
+    if (zgui.beginMainMenuBar()) {
+        if (zgui.beginMenu("Dialogue demos", true)) {
+            _ = zgui.menuItemPtr("Localization Demo", .{ .selected = &demo.is_selected });
+            zgui.endMenu();
+        }
+
+        zgui.endMainMenuBar();
+    }
+
+    if (demo.is_selected) {
+        if (zgui.begin("Localization Demo", .{ .flags = .{ .always_auto_resize = true } })) {
+            if (zgui.button("Say \"Hello\"", .{})) {}
+
+            if (zgui.beginCombo("Language", .{ .preview_value = Languages[0] })) {
+                for (Languages, 0..) |lang, i| {
+                    const is_selected = i == 0;
+                    if (zgui.selectable(lang, .{ .selected = is_selected })) {}
+                }
+
+                zgui.endCombo();
+            }
+
+            zgui.end();
+        }
+    }
+}
+
+fn draw(demo: *DemoState) void {
+    const graphics_context = demo.graphics_context;
+
+    const swapchain_texture_view = graphics_context.swapchain.getCurrentTextureView();
+    defer swapchain_texture_view.release();
+
+    const commands = commands: {
+        const encoder = graphics_context.device.createCommandEncoder(null);
+        defer encoder.release();
+
+        {
+            const pass = zgpu.beginRenderPassSimple(encoder, .load, swapchain_texture_view, null, null, null);
+            defer zgpu.endReleasePass(pass);
+            zgui.backend.draw(pass);
+        }
+
+        break :commands encoder.finish(null);
+    };
+    defer commands.release();
+
+    graphics_context.submit(&.{commands});
+    _ = graphics_context.present();
+}
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    zglfw.init() catch {
+        std.log.err("Failed to initialize GLFW library.", .{});
+        return;
+    };
+    defer zglfw.terminate();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    const window = zglfw.Window.create(1920, 1080, "wwise-zig Demo", null) catch {
+        std.log.err("Failed to create demo window", .{});
+        return;
+    };
+    defer window.destroy();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
 
-    try bw.flush(); // don't forget to flush!
+    const allocator = gpa.allocator();
+
+    const demo = try setup(allocator, window);
+    defer destroy(allocator, demo);
+
+    while (!window.shouldClose() and window.getKey(.escape) != .press) {
+        zglfw.pollEvents();
+        update(demo);
+        draw(demo);
+    }
 }
